b466b025e6a97133f706b8bfbe58c8846266b8d8
nomu0023 <no_mu23@yahoo.co.jp>
2015-03-06 22:34:45 +0900

    R.javaの修正

--- src/com/example/game1/GoalActivity.java
+++ src/com/example/game1/GoalActivity.java
@@ -1,5 +1,6 @@
 package com.example.game1;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
--- src/com/example/game1/HardActivity.java
+++ src/com/example/game1/HardActivity.java
@@ -1,15 +1,26 @@
 package com.example.game1;
 
-import android.os.Bundle;
+
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageButton;
 
 public class HardActivity extends Activity {
-
+	int i;
+	 ImageButton[] imageButtons = new ImageButton[12]  ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hard);
+		for(i=0; i<12; i++) {
+			ImageButton[] imageButton = null;
+			imageButton[i] = (ImageButton)findViewById(getResources().getIdentifier
+					("image" + i, "id", getPackageName()));
+			imageButton[i].setImageResource(R.drawable.hold);
+		}
+	
 	}
 
 	@Override
